fbb5941f209cd3eb2056bd9be4d68321e715ea4e
sudhaAndroid <39403888+sudhaAndroid@users.noreply.github.com>
2018-05-18 16:46:28 +0530

    First Date

--- collapsiblecalendarview2/src/main/java/com/shrikanthravi/collapsiblecalendarview/widget/UICalendar.java
+++ collapsiblecalendarview2/src/main/java/com/shrikanthravi/collapsiblecalendarview/widget/UICalendar.java
@@ -57,7 +57,7 @@ public abstract class UICalendar extends LinearLayout {
     protected ExpandIconView expandIconView;
 
     // Attributes
-    private boolean mShowWeek = true;  
+    private boolean mShowWeek = true;
     private int mFirstDayOfWeek = SUNDAY;
     private int mState = STATE_COLLAPSED;
     private int mTextColor = Color.BLACK;
